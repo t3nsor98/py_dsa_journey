962b20a0a03d4f425eac00894f6aacffe6d5889b
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-05-17 21:55:07 +0530

    Implement twoSum function for both sorted and unsorted arrays

---
+++ 75_Blind/two_sum.py
@@ -0,0 +1,37 @@
+def twoSum(self, arr, target):
+    # Your code here
+    n = len(arr)
+    l = 0
+    r = n - 1
+    while l < r:
+        sum = arr[l] + arr[r]
+        if sum == target:
+            return [l + 1, r + 1]
+        elif sum < target:
+            l = l + 1
+        else:
+            r = r - 1
+
+    return []
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+
+
+# for un sorted array
+def twoSum(self, arr, target):
+    # Your code here
+    sorted_arr = sorted(arr)
+    n = len(sorted_arr)
+    l = 0
+    r = n - 1
+    while l < r:
+        sum = sorted_arr[l] + sorted_arr[r]
+        if sum == target:
+            return [l + 1, r + 1]
+        elif sum < target:
+            l = l + 1
+        else:
+            r = r - 1
+
+    return []
